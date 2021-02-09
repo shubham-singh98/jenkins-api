27f675a6d4894e89af7b4c34e84ef67fb2bdff6d
SH20170450 <SH20170450@APL-5CD0177C2G>
2021-02-10 00:52:12 +0530

    first commit

---
+++ src/main/java/com/wipro/jenkinsrestapi/sample.java
@@ -0,0 +1,43 @@
+package com.wipro.jenkinsrestapi;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+public class sample {
+    public static void main(String[] args) throws Exception {
+        final Logger log = Logger.getLogger(sample.class.getCanonicalName());
+        JobData jd = new JobData();
+
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.print("What is your user name? ");
+            jd.setUsername(input.next());
+            System.out.print("What is your password? ");
+            jd.setPassword(input.next());
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "The system encountered an exception while attempting to login");
+        } finally {
+              input.close();
+        }
+
+        jd.setJob("maven");
+        jd.setServer("http://192.168.29.51");
+        jd.setPort("8080");
+        // set the parameters
+        List<NameValuePair> parameters = jd.getParameters();
+        //parameters.add(new BasicNameValuePair("SONAR-TARGET", "true"));
+        //parameters.add(new BasicNameValuePair("RELEASE", "1311.1.1"));
+        //parameters.add(new BasicNameValuePair("REVISION", "HEAD"));
+        //parameters.add(new BasicNameValuePair("description", "jenkins test run"));
+        // run the job
+        JenkinsPoster jp = new JenkinsPoster(jd);
+        log.info("executing postJenkinsJob");
+        jp.postJenkinsJob();
+        log.info("executed postJenkinsJob");
+    }   
+}
---
+++ src/main/java/jenkinsapi2/JenkinsMonitor.java
@@ -0,0 +1,91 @@
+package jenkinsapi2;
+
+import java.util.Scanner;
+
+import org.apache.http.auth.*;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+
+import com.google.gson.Gson;
+
+public class JenkinsMonitor {
+
+    public static void main(String[] args) throws Exception {
+
+        String protocol = "http";
+        String host = "192.168.29.51";
+        int port = 8080;
+        String usernName = "shubham";
+        String password = ")yz$trazG5";
+
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        httpclient.getCredentialsProvider().setCredentials(
+                new AuthScope(host, port), 
+                new UsernamePasswordCredentials(usernName, password));
+
+        String jenkinsUrl = protocol + "://" + host + ":" + port;
+
+        try {
+            // get the crumb from Jenkins
+            // do this only once per HTTP session
+            // keep the crumb for every coming request
+        	 System.out.println("Connecting to...."+jenkinsUrl);
+        	 Thread.sleep(2000);
+        	 System.out.println("Connecting to...."+jenkinsUrl);
+        	 Thread.sleep(1000);
+        	 System.out.println("Connecction established");
+        	 System.out.println();
+            System.out.println("... issue crumb");
+            HttpGet httpGet = new HttpGet(jenkinsUrl + "/crumbIssuer/api/json");
+            String crumbResponse= toString(httpclient, httpGet);
+            CrumbJson crumbJson = new Gson()
+                .fromJson(crumbResponse, CrumbJson.class);
+
+            // add the issued crumb to each request header
+            // the header field name is also contained in the json response
+            System.out.println("... issue rss of latest builds");
+            HttpPost httpost = new HttpPost(jenkinsUrl + "/rssLatest");
+            httpost.addHeader(crumbJson.crumbRequestField, crumbJson.crumb);
+            toString(httpclient, httpost);
+            
+            System.out.println();
+           
+            try {
+            	 Scanner input = new Scanner(System.in);
+            	 System.out.println("Enter the Job to build in Jenkins..");
+            	 String job=input.next();
+            	 String buildurl= String.format("/job/%s/build", job);
+            	 
+            	 
+            HttpPost httpost2 = new HttpPost(jenkinsUrl + buildurl);
+            httpost2.addHeader(crumbJson.crumbRequestField, crumbJson.crumb);
+            toString(httpclient, httpost2);
+            System.out.println("....starting to build");
+            }
+            catch(Exception e)
+            {
+            	System.out.print("Sorry something went Wrong: PLease check your job Name");
+            }
+            
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+
+    }
+
+    // helper construct to deserialize crumb json into 
+    public static class CrumbJson {
+        public String crumb;
+        public String crumbRequestField;
+    }
+
+    private static String toString(DefaultHttpClient client, 
+        HttpRequestBase request) throws Exception {
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        String responseBody = client.execute(request, responseHandler);
+        System.out.println(responseBody + "\n");
+        return responseBody;
+    }
+
+}
